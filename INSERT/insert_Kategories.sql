bc810182bdc0940404c9aa556ce04eada717a5d6
Muhamad Nur Syami <nursami385@gmail.com>
2023-11-08 22:25:01 +0700

    add insert table

---
+++ INSERT/insert_Kategories.sql
@@ -0,0 +1,12 @@
+INSERT INTO Kategories (nama_kategories, deskripsi, tanggal_dibuat)
+VALUES
+("elektronik", "lorempu10irbu", NOW()),
+("makanan", "untuk dimakan", NOW()),
+("minuman", "untuk diminum", NOW()),
+("kebersihan", "untuk bersih", NOW()),
+("pakaian", "untuk dipakai", NOW()),
+("kesehatan", "untuk sehat", NOW()),
+("olahraga", "untuk olahraga", NOW()),
+("perhiasan", "untuk hias", NOW()),
+("beras", "untuk beras", NOW()),
+("sirup", "untuk sirup", NOW());
---
+++ INSERT/insert_Pelanggan.sql
@@ -0,0 +1,14 @@
+INSERT INTO Pelanggan (nama, alamat, noHp, email)
+VALUES
+("jokowi", "jln solo", "08678267", "jokowi@gmail.com"),
+("mega", "jln banteng", "08427893", "mega@gmail.com"),
+("prabowo", "jln garuda", "08238719", "prabowo@gmail.com"),
+("ganjar", "jln pdi", "03827368", "ganjar@gmail.com"),
+("gibran", "jln solo 2", "12345678", "gibran@gmail.com"),
+("anies", "jalan jakarta", "09876543", "anies@gmail.com"),
+("amin", "jalan dki", "98675434", "amin@gmail.com"),
+("erik", "jalan pssi", "12092384", "erik@gmail.com"),
+("budi", "jalan bidu", "64528012", "budi@gmail.com"),
+("sandi", "jalan uno", "82638021", "sandi@gmail.com"),
+("nadiem", "jalan makarim", "01278364", "nadiem@gmail.com"),
+("john", "jalan roti john", "83628534", "john@gmail.com");
