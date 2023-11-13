d6faefe6f0773dc44210d99dc5f74144fdc0e169
Muhamad Nur Syami <nursami385@gmail.com>
2023-11-13 13:18:18 +0700

    perbaiki kasus 1

--- CASE/kasus1_1_pelanggan_membeli_3_barang_berbeda.sql
+++ CASE/kasus1_1_pelanggan_membeli_3_barang_berbeda.sql
@@ -1,1 +1,9 @@
+-- insert1 pelangan membeli 3 produk berbeda
+INSERT INTO Transaksi (tanggal_transaksi, jumlah_barang, total_harga, id_pelanggan, id_barang)
+VALUES
+(NOW(), 10, 50000, 1,7 ),
+(NOW(), 5, 20000, 1, 6),
+(NOW(), 20, 20000, 1,9);
+
+-- melihat 1 pelangan membeli 3 produk berbeda
 SELECT * from Transaksi where id_pelanggan = 1;
